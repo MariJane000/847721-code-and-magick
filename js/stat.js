14e255f9f7cc125702f8dccfd14e3c9467962ec9
vvs-code <vlad@veselov.spb.ru>
2018-09-04 17:41:25 +0300

    Исправил ошибки ESlint

--- js/stat.js
+++ js/stat.js
@@ -1,11 +1,11 @@
 'use strict';
 
-window.renderStatistics = function(ctx, names, times) {
+window.renderStatistics = function (ctx, names, times) {
 
-  var getRand = function (from, to) {
+  var getRand = function (from, to) {  
     return Math.floor(Math.random() * (to - from + 1)) + from;
-  }
-
+  };
+  
   var namesNum = names.length;
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(110, 20, 420, 270);
@@ -16,7 +16,7 @@ window.renderStatistics = function(ctx, names, times) {
   ctx.fillText('Ура вы победили!', 130, 40);
   ctx.fillText('Список результатов:', 130, 60);
 
-  var maxNumInArray = function(arr) {
+  var maxNumInArray = function (arr) {
     var max = arr[0];
     for (var i = 1; i < arr.length; i++) {
       if (arr[i] > max) {
@@ -24,7 +24,7 @@ window.renderStatistics = function(ctx, names, times) {
       }
     }
     return max;
-  }
+  };
 
   var maxTime = maxNumInArray(times);
 
@@ -36,14 +36,14 @@ window.renderStatistics = function(ctx, names, times) {
     ctx.fillText(thisName, 155 + (90 * i), 260);
     ctx.fillText(Math.round(thisTime), 155 + (90 * i), 80 + (150 - columnHeight));
 
-    if (thisName == "Вы") {
+    if (thisName === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
-      var randomSaturation = getRand(0, 128);
+      var randomSaturation = getRand (0, 128);
       ctx.fillStyle = 'rgba(' + randomSaturation + ', ' + randomSaturation + ', ' + (255 - randomSaturation) + ', 1)';
     }
 
     ctx.fillRect(155 + (90 * i), 90 + (150 - columnHeight), 40, columnHeight);
   }
 
-}
+};
