20b2e9bf656647fb26e61a8ec1f312299f9bece8
vvs-code <vlad@veselov.spb.ru>
2018-09-09 16:32:34 +0300

    Сделал правки 2 урок + ESLint

--- js/stat.js
+++ js/stat.js
@@ -8,14 +8,14 @@ window.renderStatistics = function (ctx, names, times) {
   var RECT_Y_OFFSET = 10;
   var TITLE_X_OFFSET = 130;
   var TITLE_Y_OFFSET = 40;
-  
+
   var getRandom = function (from, to) {
     return Math.floor(Math.random() * (to - from + 1)) + from;
   };
 
   var namesNum = names.length;
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(RECT_X_OFFSET + 10 , RECT_Y_OFFSET + 10, RECT_WIDTH, RECT_HEIGHT);
+  ctx.fillRect(RECT_X_OFFSET + 10, RECT_Y_OFFSET + 10, RECT_WIDTH, RECT_HEIGHT);
   ctx.fillStyle = 'rgba(255, 255, 255, 1)';
   ctx.fillRect(RECT_X_OFFSET, RECT_Y_OFFSET, RECT_WIDTH, RECT_HEIGHT);
   ctx.fillStyle = 'rgba(0, 0, 0, 1)';
@@ -46,10 +46,10 @@ window.renderStatistics = function (ctx, names, times) {
     if (playerName === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
-	    // Здесь использую hsl, потому что в hsl удобно работать с насыщенностью.
+      // Здесь использую hsl, потому что в hsl удобно работать с насыщенностью.
       ctx.fillStyle = 'hsl(240, ' + getRandom(0, 100) + '%, 50%)';
     }
-    
+
     ctx.fillRect(155 + (90 * i), 90 + (150 - columnHeight), 40, columnHeight);
   }
 
