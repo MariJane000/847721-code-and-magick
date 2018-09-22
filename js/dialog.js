0e81cd5abae6aaa6f57f8ea452b343f362ad796c
vvs-code <vlad@veselov.spb.ru>
2018-09-22 12:26:17 +0300

    Правки по валидации формы

---
+++ js/dialog.js
@@ -0,0 +1,57 @@
+'use strict';
+
+(function () {
+
+  var userPic = document.querySelector('.upload');
+  var setup = document.querySelector('.setup');
+
+  userPic.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var dragged = false;
+
+    var onMouseMove = function (moveEvt) {
+      dragged = true;
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      if ((shift.x > 5 || shift.x < -5) || (shift.y > 5 || shift.y < -5)) {
+        moveEvt.preventDefault();
+        setup.style.top = (setup.offsetTop - shift.y) + 'px';
+        setup.style.left = (setup.offsetLeft - shift.x) + 'px';
+      }
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+
+      if (dragged) {
+        var onClickPreventDefault = function (evt) {
+          evt.preventDefault();
+          userPic.removeEventListener('click', onClickPreventDefault);
+        };
+        userPic.addEventListener('click',
+          onClickPreventDefault);
+      }
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+
+  });
+})();
