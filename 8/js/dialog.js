57625f279b1393dcd342559088818adf4c41dac6
Keks <keks@htmlacademy.ru>
2018-09-25 00:06:33 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/dialog.js
@@ -0,0 +1,53 @@
+'use strict';
+
+window.dialogDragAndDrop = function (evt) {
+
+  var userPic = document.querySelector('.upload');
+  var setup = document.querySelector('.setup');
+
+  evt.preventDefault();
+
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  var dragged = false;
+
+  var onMouseMove = function (moveEvt) {
+    dragged = true;
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    if ((shift.x > 5 || shift.x < -5) || (shift.y > 5 || shift.y < -5)) {
+      moveEvt.preventDefault();
+      setup.style.top = (setup.offsetTop - shift.y) + 'px';
+      setup.style.left = (setup.offsetLeft - shift.x) + 'px';
+    }
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+
+    if (dragged) {
+      var onClickPreventDefault = function (clickEvt) {
+        clickEvt.preventDefault();
+        userPic.removeEventListener('click', onClickPreventDefault);
+      };
+      userPic.addEventListener('click', onClickPreventDefault);
+    }
+  };
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+};
